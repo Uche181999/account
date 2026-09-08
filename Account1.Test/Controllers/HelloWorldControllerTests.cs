7d96e64e034b726b673cb477fa176df4754eab76
Emmanuel uche <uche181999@gmail.com>
2026-09-08 15:05:35 +0100

    added helloworld and helloworld test

---
+++ Account1.Test/Controllers/HelloWorldControllerTests.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using Account1.Controllers;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account1.Test.Controllers
+{
+    public class HelloWorldControllerTests
+    {
+        private readonly HelloWorldController _controller ;
+        public HelloWorldControllerTests()
+        {
+            _controller = new HelloWorldController(NullLogger<HelloWorldController>.Instance);
+        }
+
+        [Fact]
+        public void GetReturnHelloWorld()
+        {
+            var result = _controller.Get() as OkObjectResult;
+            Assert.NotNull(result);
+            Assert.Equal("hello world", result!.Value);
+        }
+
+        [Fact]
+        public void GetReturnHelloWorldWithName()
+        {
+            var result = _controller.Get("uche") as OkObjectResult;
+            Assert.NotNull(result);
+            Assert.Equal("hello uche", result!.Value);
+        }
+    }
+}
---
+++ Account1/Controllers/HelloWorldController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Account1.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HelloWorldController : ControllerBase
+    {
+       private readonly ILogger<HelloWorldController> _logger;
+       public HelloWorldController(ILogger<HelloWorldController> logger)
+        {
+            _logger = logger;
+        }
+       [HttpGet]
+       public IActionResult Get()
+        {
+            _logger.LogInformation("helloWorld GET endpoint hit at time : {Time}", DateTime.Now);
+            return Ok("hello world");
+        }
+        [HttpGet("{name}")]
+       public IActionResult Get(string name)
+        {
+            _logger.LogInformation("helloWorld GET endpoint with name ({Name}) hit at time : {Time}",name, DateTime.Now);
+            return Ok($"hello {name}");
+        }
+    }
+}
